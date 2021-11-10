58de1f7bd266981e98b327433a2accfbfa2f39ea
Pieter Wigboldus <pieter.wigboldus@pon.com>
2021-11-10 15:18:08 +0100

    Add sonarcloud
    for coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,15 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json'],
+  moduleFileExtensions: ['js', 'cjs', 'mjs', 'jsx', 'json'],
 
   transform: {
-      '^.+\\.js?$': 'babel-jest',
+    '^.+\\.js?$': 'babel-jest',
+    '^.+\\.mjs?$': 'babel-jest'
   },
 
   transformIgnorePatterns: ['node_modules/(?!(@hckrnews|@ponbike)/)'],
 
   moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
 
   testMatch: ['**/__tests__/*.js'],
@@ -16,5 +17,22 @@ module.exports = {
   testURL: 'http://localhost/',
 
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js'],
-};
+  collectCoverageFrom: ['src/**/*.js', 'src/**/*.cjs', 'src/**/*.mjs'],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 100,
+      lines: 95,
+      statements: 95
+    }
+  },
+  reporters: [
+    'default',
+    [ 'jest-junit', {
+      outputDirectory: 'test-reports',
+      outputName: 'jest-junit.xml',
+    } ]
+  ],
+
+  testResultsProcessor: 'jest-sonar-reporter'
+}
